cc3fb0408ceee6e4d854963997c761e863a84ae8
Victor Rojas <vicrojas_mx@hotmail.com>
2024-05-21 08:34:44 -0400

    Moving hardcoded guest credentials to env variables

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,8 +27,8 @@ export const Login = () => {
   }
   
   async function handleLoginGuest() {
-    email.current.value= "dani.rojas@outlook.com";
-    password.current.value = "dani1234";
+    email.current.value= process.env.REACT_APP_GUEST_LOGIN;
+    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
     try {
       const authDetail = {
         email: email.current.value,
